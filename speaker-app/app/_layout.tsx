d7586e2b7a0b37ef01fc17352dea655ebc5e069e
Oleg <ipz223_soi@student.ztu.edu.ua>
2026-05-27 17:53:28 +0300

    feat: added fonts

--- speaker-app/app/_layout.tsx
+++ speaker-app/app/_layout.tsx
@@ -1,6 +1,33 @@
-import "../global.css"
+import "../global.css";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
 import { Stack } from "expo-router";
+import { useEffect } from "react";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const [loaded, error] = useFonts({
+    SF_Pro_Display_Bold: require("../assets/fonts/SF_Pro_Display_Bold.otf"),
+    SF_Pro_Display_Heavy_Italic: require("../assets/fonts/SF_Pro_Display_Heavy_Italic.otf"),
+    SF_Pro_Display_Light_Italic: require("../assets/fonts/SF_Pro_Display_Light_Italic.otf"),
+    SF_Pro_Display_Medium: require("../assets/fonts/SF_Pro_Display_Medium.otf"),
+    SF_Pro_Display_Regula: require("../assets/fonts/SF_Pro_Display_Regula.otf"),
+    SF_Pro_Display_Semibold_Italic: require("../assets/fonts/SF_Pro_Display_Semibold_Italic.otf"),
+    SF_Pro_Display_Thin_Italic: require("../assets/fonts/SF_Pro_Display_Thin_Italic.otf"),
+    SF_Pro_Display_Ultralight_Italic: require("../assets/fonts/SF_Pro_Display_Ultralight_Italic.otf"),
+    SF_Pro_Italic: require("../assets/fonts/SF_Pro_Italic.otf"),
+  });
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
+
   return <Stack />;
 }
--- speaker-app/app/index.tsx
+++ speaker-app/app/index.tsx
@@ -1,9 +1,15 @@
 import { Text, View } from "react-native";
+import { Appearance, useColorScheme } from 'react-native';
 
 export default function Index() {
   return (
+    let colorScheme = useColorScheme();
+
+
+    return (
     <View>
       <Text className='text-3xl font-bold'>Edit app/index.tsx to edit this screen.</Text>
+      <Text  className='text-body-large color-neutral-950 font-bold'>Typography - SF Pro Text</Text>
     </View>
   );
 }
