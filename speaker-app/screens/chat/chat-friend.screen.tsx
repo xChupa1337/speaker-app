129d20b70685e6546d05bb3755990e37ce8832fd
Oleg <ipz223_soi@student.ztu.edu.ua>
2026-05-30 11:09:10 +0300

    feat: updated chat-friends screen

--- speaker-app/screens/chat/chat-friend.screen.tsx
+++ speaker-app/screens/chat/chat-friend.screen.tsx
@@ -1,27 +1,40 @@
-import React from 'react';
-import {Text, View} from "react-native";
+import React from "react";
+import { Alert, ScrollView, Text, View } from "react-native";
 import useTheme from "@/store/theme";
-import {FiendFriendsDark, FiendFriendsLight} from "@/assets/images/onboarding/hello_img";
+import {
+  FiendFriendsDark,
+  FiendFriendsLight,
+} from "@/assets/images/onboarding/hello_img";
+import Button from "@/components/ui/button";
 
 const ChatFriendScreen = () => {
-    const {isDarkMode} = useTheme()
-
-    return (
-      <View className='mt-6'>
-          {isDarkMode ?
-          <FiendFriendsDark />
-              :
-              <FiendFriendsLight />
-          }
-          <View className='mt-5 max-w-[270px]'>
-              <Text
-                  className={`text-headline-small${isDarkMode ? 'text-body-primary-dark' : 'text-body-primary-light '}`}
-                  style={{textAlign: 'center'}}>
-                    Make friends with a native speaker and improve your English
-              </Text>
-          </View>
-      </View>
+  const { isDarkMode } = useTheme();
+  const handlePress = () => {
+    Alert.alert(
+      "Oops...",
+      "Sorry, adding friends is not available yet. Stay tuned for updates!",
     );
+  };
+  return (
+    <ScrollView className="pt-6">
+      {isDarkMode ? <FiendFriendsDark /> : <FiendFriendsLight />}
+      <View className="mt-5 gap-4">
+        <Text
+          className={`text-headline-small ${isDarkMode ? "text-bg-light" : "text-bg-dark "}`}
+          style={{ textAlign: "center" }}
+        >
+          Make friends with a native speaker and improve your English
+        </Text>
+        <Text
+          className={`text-body-medium ${isDarkMode ? "text-bg-light" : "text-bg-dark "} text-center`}
+        >
+          It's kind of quiet here... Add friends! You can check each other's
+          exercises and help in learning the language
+        </Text>
+        <Button onPress={handlePress}>Find friends</Button>
+      </View>
+    </ScrollView>
+  );
 };
 
 export default ChatFriendScreen;
